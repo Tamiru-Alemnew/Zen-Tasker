3ca80a31fb4eb9e71a08f9e3b968e14683fbeef1
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-07 15:01:18 +0300

    create(controllers) : to map the http request with the actual services

---
+++ controllers/task_controller.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+    "net/http"
+    "strconv"
+    "github.com/Tamiru-Alemnew/task-manager/data"
+    "github.com/Tamiru-Alemnew/task-manager/models"
+    "github.com/gin-gonic/gin"
+)
+
+func GetTasks(c *gin.Context) {
+    tasks := data.GetAllTasks()
+    c.JSON(http.StatusOK, tasks)
+}
+
+func GetTask(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid task ID"})
+        return
+    }
+    task, err := data.GetTaskByID(id)
+    if err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": "task not found"})
+        return
+    }
+    c.JSON(http.StatusOK, task)
+}
+
+func CreateTask(c *gin.Context) {
+    var task models.Task
+    if err := c.ShouldBindJSON(&task); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    createdTask := data.CreateTask(task)
+    c.JSON(http.StatusCreated, createdTask)
+}
+
+func UpdateTask(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid task ID"})
+        return
+    }
+    var task models.Task
+    if err := c.ShouldBindJSON(&task); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    updatedTask, err := data.UpdateTask(id, task)
+    if err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        return
+    }
+    c.JSON(http.StatusOK, updatedTask)
+}
+
+func DeleteTask(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid task ID"})
+        return
+    }
+    err = data.DeleteTask(id)
+    if err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        return
+    }
+    c.Status(http.StatusNoContent)
+}
